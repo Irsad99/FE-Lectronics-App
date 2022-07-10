0667d9ebe43ede0a962342c2efd14fb25fac50db
devriwirlandika <80185253+depri11@users.noreply.github.com>
2022-07-10 19:24:00 +0700

    slicing cards

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
 import style from './navbar.module.css'
-import {Navbar, Container, Nav, Button} from 'react-bootstrap'
+import { Navbar, Container, Button, InputGroup, FormControl } from 'react-bootstrap'
 import Logo from './Logo2.png'
 
 function Header() {
     return (
         <>
-        <Navbar className={style.navbar} variant="light">
-            <Container>
-            <Navbar.Brand href="#home">
+            <div className={style.navbar} />
+            <Navbar className="pt-5" style={{ margin: '0 0 5.8%' }}>
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <Button className={style.but_back}>
+                            <img src="https://i.ibb.co/HDFThwz/Vector-64.png" alt="" />
+                        </Button>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Button className={style.but_user}>
+                            <img src="https://i.ibb.co/C7HFPXt/Vector.png" alt="" />
+                        </Button>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <div className="head">
+                <Container>
+                    <InputGroup className="mb-3">
+                        <FormControl className={style.input_search} placeholder="Tap To Search For Something" aria-label="Tap To Search For Something" aria-describedby="basic-addon2" />
+                        <Button className={style.btn_search} id="button-addon2">
+                            <img src="https://i.ibb.co/PWtqWHZ/Vector.png" alt="" />
+                        </Button>
+                        <div className={style.but_alt}>
+                            <Button className={style.btn_w} style={{ margin: '0 20px 0 20px' }}>
+                                <img src="https://i.ibb.co/CK2X9RQ/Vector.png" alt="" />
+                            </Button>
+                            <Button className={style.btn_w}>
+                                <img src="https://i.ibb.co/MhwpH7b/Vector.png" alt="" />
+                            </Button>
+                        </div>
+                    </InputGroup>
+                </Container>
+            </div>
+
+            {/* <Navbar.Brand href="#home">
                 <img width="auto" height="50px" className={style.logo} src={Logo}  alt="logo" />
             </Navbar.Brand>
             <Nav className={style.nav}>
@@ -18,10 +52,10 @@ function Header() {
             <Nav.Link href="#pricing">About</Nav.Link>
             <Button className={style.button} variant="primary">Sign Up</Button>
             </Nav>
-            </Container>
-        </Navbar>
+            </Container> */}
+            {/* </Navbar> */}
         </>
-    )   
+    )
 }
 
 export default Header
---
+++ src/pages/products/products.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header/navbar'
+import Cards from '../../components/card/card'
+
+function Products() {
+    return (
+        <>
+            <Header />
+            <Cards />
+        </>
+    )
+}
+
+export default Products
